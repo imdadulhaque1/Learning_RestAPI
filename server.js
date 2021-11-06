ccca4bf312efe1ae73553db0e73fb416718ebf78
imdadulhaque1 <imdadulhaqueimdad820@gmail.com>
2021-11-06 18:11:21 +0600

    Created Server just add users site!

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ app.get('/posts', (req, res) =>{
     res.send("<h1>Stroed the all of posts!</h1>")
 })
 
+app.get('/users', (req, res) =>{
+    res.send("<ul><li>Imdadul Haque</li><li>Israt Jahan Maisha</li></ul>")
+})
+
 app.listen(PORT, () =>{
     console.log(`Server is running on PORT ${PORT}`);
 });
