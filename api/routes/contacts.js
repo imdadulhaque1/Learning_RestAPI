b853a68eb8da4413d13a796007dc1e77bb3e9f35
imdadulhaque1 <imdadulhaqueimdad820@gmail.com>
2021-11-10 11:37:23 +0600

    Express Route and use od GET, PUT, POST, DELETE

--- api/routes/contacts.js
+++ api/routes/contacts.js
@@ -9,12 +9,34 @@ router.get('/', (req, res, next) =>{
     })
 })
 
-router.get('/:id', (req, res, next) =>{
-    // console.log(req.url)
-    const id = req.params.id      //to Extract the id should use: req.params.id
-    res.json({
-        id
+// router.get('/:id', (req, res, next) =>{
+//     // console.log(req.url)
+//     const id = req.params.id      //to Extract the id should use: req.params.id
+//     res.json({
+//         id
+//     })
+// })
+
+// Post Method
+router.post('/:id', (req, res, next) =>{
+    res.status(201).json({
+        message: "Hello, I am POST ROUTE from api/routes/contacts!"
     })
 })
+
+// PUT Method
+router.put('/:id', (req, res, next) =>{
+    res.json({
+        message: "Hello, I am PUT ROUTE from api/routes/contacts!"
+    })
+})
+
+// DELETE Method
+router.delete('/:id', (req, res, next) =>{
+    res.json({
+        message: "Hello, I am DELETE ROUTE from api/routes/contacts!"
+    })
+})
+
 
 module.exports = router
