44578cce2eaa9afa91d142351c3854b936d882c5
imdadulhaque1 <imdadulhaqueimdad820@gmail.com>
2021-11-13 15:23:01 +0600

    CORS define in server.js file

--- api/routes/contacts.js
+++ api/routes/contacts.js
@@ -1,6 +1,6 @@
-// const express = require('express').Router();
-const express = require('express');
-const router = express.Router();
+// const express = require('express').Router()
+const express = require('express')
+const router = express.Router()
 
 // Store the data in contacts array
 const contacts = []
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 
 // Connects routes
@@ -8,6 +9,8 @@ const constactsRoute = require('./api/routes/contacts')
 
 const app = express();
 app.use(morgan('dev'))
+
+app.use(cors)
 
 // app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.urlencoded({ extended: true}))
